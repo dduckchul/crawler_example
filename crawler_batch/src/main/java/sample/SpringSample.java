0d35d77bc2df588dbe867220c587518681658766
DDuckchul <dduckchul@naver.com>
2019-09-26 09:02:30 +0900

    # 스프링 배치 Example 적용

---
+++ crawler_batch/src/main/java/sample/SpringSample.java
@@ -0,0 +1,22 @@
+package sample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+import test.Person;
+
+public class SpringSample implements ItemProcessor<Person, Person> {
+    private static final Logger log = LoggerFactory.getLogger(SpringSample.class);
+
+    @Override
+    public Person process(final Person person) throws Exception {
+        final String firstName = person.getFirstName().toUpperCase();
+        final String lastName = person.getLastName().toUpperCase();
+
+        final Person transformedPerson = new Person(firstName, lastName);
+
+        log.info("Converting (" + person + ") into (" + transformedPerson + ")");
+
+        return transformedPerson;
+    }
+}

748bece2591cec6af056c1d7d74c6241f8fdfceb
DDuckchul <dduckchul@naver.com>
2019-10-08 00:30:39 +0900

    #패키지 변경 / 불필요파일 제거

--- crawler_common/src/main/java/sample/Person.java
+++ crawler_common/src/main/java/sample/Person.java
@@ -1,4 +1,4 @@
-package test;
+package sample;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- crawler_common/src/main/java/test/TestDto.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import lombok.Data;
-
-@Data
-public class TestDto {
-
-    private String data;
-}
